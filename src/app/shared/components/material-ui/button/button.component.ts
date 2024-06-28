3f7d37648bfb933ace20544ca524341622e70bab
Skeinc <skezydeveloper@gmail.com>
2024-06-28 23:57:08 +0300

    Add button component

---
+++ src/app/shared/components/material-ui/button/button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ui-button',
+    templateUrl: './button.component.html',
+    styleUrl: './button.component.scss',
+})
+export class ButtonComponent {}
